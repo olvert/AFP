cfff89f90cd85ca9bf614db86053b61d713df27f
Johannes <johannes.ekengren@gmail.com>
2017-02-12 22:30:18 +0100

    ass2 first hand in, not working correctly

--- ass2/replay.hs
+++ ass2/replay.hs
@@ -11,6 +11,8 @@ module Replay (
 
 import Control.Applicative
 import Control.Monad (liftM, ap)
+import Data.Time
+
 
 -- Types
 data Replay q r a where
@@ -36,6 +38,7 @@ instance Applicative (Replay q r) where
   pure  = return
   (<*>) = ap
 
+
 io  :: (Show a, Read a) => IO a -> Replay q r a
 io =  IO
 
@@ -52,15 +55,56 @@ addResult  :: Show s => Trace r -> s -> Trace r
 addResult t s = t ++ [Result $ show s]
 
 
-run :: Replay q r a -> Trace r -> IO (Either (q, Trace r) a) -- typechecks but does not do what we want..
-run (IO a) []             = do  a' <- a
-                                return (Right a')
-run (IO a) (Result t:ts)  = return (Right $ read t)
-run (Ask q) t             = return (Left(q, t))
-run (Return a) t          = return (Right a)
-run (Bind r f) t          = do  r' <- run r t
-                                case r' of
-                                  (Right a)     -> run (f a) t
-                                  (Left (q,t))  -> return $ Left (q,t)
 
-run _ _                   = error "No match in pattern"
+
+
+-- run :: Replay q r a -> Trace r -> IO (Either (q, Trace r) a) -- typechecks but does not do what we want..
+-- run (IO a) []             = do  a' <- a
+--                                 return $ Right a'
+-- run (IO a) (Result t:ts)  = return $ Right $ read t
+-- run (Ask q) (Answer a:ts) = return $ Right a
+-- run (Ask q) t             = return $ Left (q, t)
+-- run (Return a) t          = return (Right a)
+-- run (Bind r f) t          = do r' <- run r t
+--                                case r' of
+--                                  (Right a)      -> run (f a) t
+--                                  (Left (q, t')) -> return $ Left (q, t')
+-- run _ _                   = error "No match in pattern"
+
+
+
+run :: Replay q r a -> Trace r -> IO (Either (q, Trace r) a)
+run r t = convert $ run' r t
+
+run' :: Replay q r a -> Trace r -> IO (Either (q, Trace r) (a, Item r))
+run' (IO a) []             = do a' <- a
+                                return $ Right (a', Result ( show a'))
+run' (IO a) (Result t:ts)  = return $ Right (read t, Result (show t))
+run' (Ask q) (Answer a:ts) = return $ Right (a, Answer a)
+run' (Ask q) t             = return $ Left (q, t)
+run' (Return a) t          = return $ Right (a, Result "")
+run' (Bind r f) t          = do r' <- run' r t
+                                case r' of
+                                  (Right (a, Result r)) -> run' (f a) (addResult t r)
+                                  (Right (a, Answer r)) -> run' (f a) (addAnswer t r)
+                                  (Left (q, t'))  -> return $ Left (q, t')
+run' _ _                   = error "No match in pattern"
+
+convert :: IO (Either (q, Trace r) (a, Item r)) -> IO (Either (q, Trace r) a)
+convert e = do e' <- e
+               case e' of
+                 (Left (q, t))  -> return $ Left (q, t)
+                 (Right (a, i)) -> return $ Right a
+
+
+example :: Replay String String Int
+example = do
+  t0 <- io getCurrentTime
+  io (putStrLn "Hello!")
+  age <- ask "What is your age?"
+  io (putStrLn ("You are " ++ age))
+  name <- ask "What is your name?"
+  io (putStrLn (name ++ " is " ++ age ++ " years old"))
+  t1 <- io getCurrentTime
+  io (putStrLn ("Total time: " ++ show (diffUTCTime t1 t0)))
+  return (read age)
