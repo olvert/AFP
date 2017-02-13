b8ffcc94892479b267785db8f794adf4a1f7aebb
Olle Svensson <olle@kent.nu>
2017-02-13 12:57:25 +0100

    Completed first working messy version

--- ass2/main.hs
+++ ass2/main.hs
@@ -4,7 +4,7 @@ import Data.Time
 import Replay
 
 main :: IO ()
-main = do run example emptyTrace
+main = do x <- running example
           return ()
 
 
@@ -19,3 +19,18 @@ example = do
   t1 <- io getCurrentTime
   io (putStrLn ("Total time: " ++ show (diffUTCTime t1 t0)))
   return (read age)
+
+exTrace :: Trace String
+exTrace = [Result "2017-02-13 10:36:13.434241 UTC",Result "()", Answer "27"]
+
+running :: Replay String String a -> IO a
+running prog = play emptyTrace
+ where
+  play t = do
+    r <- run prog t    -- this is the same prog every time!
+    case r of
+      Left (q,t2) -> do
+        putStr ("Question: " ++ q ++ " ")
+        r <- getLine
+        play (addAnswer t2 r)
+      Right x -> return x
--- ass2/replay.hs
+++ ass2/replay.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE GADTs, FlexibleInstances #-}
 module Replay (
-  Replay, Trace,
+  Replay, Trace, Item (Result, Answer),
 
   io, ask,
 
@@ -48,19 +48,33 @@ emptyTrace = []
 addAnswer  :: Trace r -> r -> Trace r
 addAnswer t a = t ++ [Answer a]
 
-addResult  :: Show s => Trace r -> s -> Trace r
-addResult t s = t ++ [Result $ show s]
+addResult  :: Trace r -> String -> Trace r
+addResult t s = Result s : t
 
+-- TESTING
 
-run :: Replay q r a -> Trace r -> IO (Either (q, Trace r) a) -- typechecks but does not do what we want..
-run (IO a) []             = do  a' <- a
-                                return (Right a')
-run (IO a) (Result t:ts)  = return (Right $ read t)
-run (Ask q) t             = return (Left(q, t))
-run (Return a) t          = return (Right a)
-run (Bind r f) t          = do  r' <- run r t
-                                case r' of
-                                  (Right a)     -> run (f a) t
-                                  (Left (q,t))  -> return $ Left (q,t)
+newtype T q r a = T (Either q a, Trace r)
 
-run _ _                   = error "No match in pattern"
+interp :: Replay q r a -> Trace r -> IO (T q r a)
+interp (IO a) []               = do a' <- a; return $ T (Right a', [Result $ show a'])
+interp (IO a) (Result s : ts)  = return $ T (Right $ read s, [Result s])
+interp (Ask q) []      = return $ T (Left q, [])
+interp (Ask q) (Answer r : ts)      = return $ T (Right r, [Answer r])
+interp (Return a) _   = return $ T (Right a, [])
+interp (Bind r f) ts   = do   r' <- interp r ts
+                              case r' of
+                                T (Right a, ts')  -> do T (t, ts'') <- interp (f a) (tail' ts)
+                                                        return $ T (t, ts' ++ ts'')
+                                T (Left q, ts') -> return $ T (Left q, ts)
+
+interp _ _ = error "No match in pattern!"
+
+tail' :: [a] -> [a]
+tail' []      = []
+tail' as      = tail as
+
+run :: Replay q r a -> Trace r -> IO (Either (q, Trace r) a)
+run r t = do  r' <- interp r t
+              case r' of
+                T (Right a, t) -> return (Right a)
+                T (Left q, t) -> return (Left (q,t))
