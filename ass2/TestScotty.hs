fbeabebf16ca70741bebf4a7d5606e956665a943
Olle Svensson <olle@kent.nu>
2017-03-05 16:33:14 +0100

    Added simple form validation

--- ass2/TestScotty.hs
+++ ass2/TestScotty.hs
@@ -12,19 +12,21 @@ main = scotty 3000 $ do
     get "/" (runWeb example)
     post "/" (runWeb example)
 
-
 example :: Web ()
 example = do
-  let as = [("people", "10"), ("color", "magenta"), ("calc", "1361")]
+  let qi1 = ("people", Num)
+      qi2 = ("color", String)
+      qi3 = ("calc", Num)
+      as = [(qi1, "10"), (qi2, "magenta"), (qi3, "1361")]
   guesses <- ask [
-    ("people", "How many million people lives in Sweden?"), 
-    ("color", "What color do you get if you blend blue and red light?"),
-    ("calc", "What is the next prime number after 1337?")]
-  let (r, w) = getScore as guesses 
+    (qi1, "How many million people lives in Sweden?"),
+    (qi2, "What color do you get if you blend blue and red light?"),
+    (qi3, "What is the next prime number after 1337?")]
+  let (r, w) = getScore as guesses
   io (putStrLn ("You scored " ++ show r ++ "/" ++ show (r + w)))
-  ask [("result", "You scored " ++ show r ++ "/" ++ show (r + w))]
+  ask [(("result", String), "You scored " ++ show r ++ "/" ++ show (r + w))]
   return ()
 
-getScore :: [(String, String)] -> [(String, String)] -> (Int, Int)
-getScore as qs = (length (filter (== True) t), length (filter (== False) t)) 
+getScore :: Answers -> Answers -> (Int, Int)
+getScore as qs = (length (filter (== True) t), length (filter (== False) t))
   where t = [ lookup k as == Just vq | (k, vq) <- qs ]
--- ass2/WebForm.hs
+++ ass2/WebForm.hs
@@ -1,10 +1,11 @@
--- | A web interface using the Replay monad 
+-- | A web interface using the Replay monad
 -- with Form as questions and Answers as trace.
-module WebForm (Web, Form, Answers, runWeb) where
+module WebForm (Web, Form, Answers, InputType (String, Num), QInfo, runWeb) where
 
 import Replay
 import Web.Scotty
 import Data.Text.Lazy (Text, pack)
+import Data.Char
 import Control.Monad.IO.Class
 import Codec.Binary.Base64.String
 
@@ -19,65 +20,110 @@ type Form = [FQuestion]
 
 -- | Type synonym for a pair with an ID and a string, where ID is an identifier
 -- and String is the question in text.
-type FQuestion = (ID, String)
+type FQuestion = (QInfo, String)
 
 -- | Type synonym for a list of form answers, FAnswer.
 type Answers = [FAnswer]
 
+type QInfo = (ID, InputType)
+
 -- | Type synonym for a pair with an ID and a string, where ID is an identifier
 -- and String is the answer in text.
-type FAnswer = (ID, String)
+type FAnswer = (QInfo, String)
 
--- | Takes a 'Web' and returns a 'ActionM' based on that monad. 
+data InputType = String | Num
+  deriving (Show, Read, Eq)
+
+
+type EitherWeb = Either (Form, Trace Answers) ()
+
+-- | Takes a 'Web' and returns a 'ActionM' based on that monad.
 runWeb :: Web () -> ActionM ()
 runWeb web = do
-  t <- trace
-  liftIO $ print t
-  --  liftIO $ print $ parseTrace t
-  r <- liftIO $ run web t
-  case r of
-    Left (f, t') -> serve f t'
-    Right x      -> liftIO $ putStrLn "Done!"
+  (prev, curr) <- trace
+  r <- liftIO $ run web $ nextTrace prev curr
+  html $ nextHtml curr r
 
-trace :: ActionM (Trace Answers)
+  where
+    nextHtml :: Maybe (Item Answers) -> EitherWeb -> Text
+    nextHtml _ (Right x) = serveResult
+    nextHtml Nothing (Left (f, t)) = serveForm f t
+    nextHtml (Just as) (Left (f, t)) = serveFormE f t $ validate as
+
+    nextTrace :: Trace Answers -> Maybe (Item Answers) -> Trace Answers
+    nextTrace t Nothing = t
+    nextTrace t (Just as) | and $ validate as = t ++ [as]
+                          | otherwise = t
+
+trace :: ActionM (Trace Answers, Maybe (Item Answers))
 trace = do
-  rawTrace        <- getRawTrace
-  formInput       <- getRawFormInput
-  liftIO $ putStrLn $ "Form input: " ++ formInput
-  let parsedTs = parseTrace rawTrace in
-    case formInput of
-      [] -> return parsedTs
+  rawTrace    <- getRawTrace
+  rawFormInfo <- getRawFormInfo
+  let prev = parseTrace rawTrace in
+    case rawFormInfo of
+      [] -> return (prev, Nothing)
       _  -> do
-        parsedT <- parseFormInputValues $ parseFormInputNames formInput
-        return $ parsedTs ++ [parsedT]
+        liftIO $ print $ parseFormInputInfo rawFormInfo
+        curr <- getFormInputValues $ parseFormInputInfo rawFormInfo
+        return (prev, Just curr)
 
--- | Serves a ActionM monad based on a form with a corresponding trace.
-serve :: Form -> Trace Answers -> ActionM ()
-serve f t = html $
-            pack $
-            unlines $
-            wrapBodyHtml $
-            concat [ getFormHtml f
-                   , getOKHtml
-                   , getTraceHtml t
-                   , getFormInputHtml f]
+validate :: Item Answers -> [Bool]
+validate (Result s) = [True]
+validate (Answer as) = map valid as
+  where
+    valid :: FAnswer -> Bool
+    valid ((_, String), a) = True
+    valid ((_, Num), a) = all isNumber a
 
--- | Surround a list of Strings with the html tags: html, body, form with 
+preProcess :: [String] -> Text
+preProcess ss = pack $ unlines $ wrapBodyHtml ss
+
+-- | Serves an ActionM monad based on a form with a corresponding trace.
+serveForm :: Form -> Trace Answers -> Text
+serveForm f t = serveFormE f t $ replicate (length f) True
+
+serveFormE :: Form -> Trace Answers -> [Bool] -> Text
+serveFormE f t bs = preProcess $
+                    wrapFormHtml $
+                    concat [ getFormHtmlE f bs
+                           , getOKHtml
+                           , getTraceHtml t
+                           , getFormInputHtml f
+                           , getErrorHtml $ and bs]
+
+serveResult :: Text
+serveResult = undefined
+
+-- | Surround a list of Strings with the html tags: html, body, form with
 -- method attribute of type post.
 wrapBodyHtml :: [String] -> [String]
 wrapBodyHtml ss = concat [prepend, ss, append]
-  where prepend = ["<html><body>", "<form method=post>"]
-        append  = ["</form>", "</body></html>"]
+  where prepend = ["<html><body>"]
+        append  = ["</body></html>"]
 
--- | Convert a Form to its html representaion.
-getFormHtml :: Form -> [String]
-getFormHtml = concatMap getQuestionHtml
+wrapFormHtml :: [String] -> [String]
+wrapFormHtml ss = concat [prepend, ss, append]
+  where prepend = ["<form method=post>"]
+        append  = ["</form>"]
+
+getErrorHtml :: Bool -> [String]
+getErrorHtml True  = []
+getErrorHtml False =
+  [ "<p><i>Your input contained errors.</i></p>"
+  , "<p><i>You are therefor being scolded accordingly.</i></p>"]
+
+getFormHtmlE :: Form -> [Bool] -> [String]
+getFormHtmlE f bs = concatMap getQuestionHtmlE $ zip f bs
 
 -- | Convert a FQuestion to its html representaion.
+getQuestionHtmlE :: (FQuestion, Bool) -> [String]
+getQuestionHtmlE ((i, q), v) = if v then [question, input] else [questionE, input]
+  where question  = concat ["<p>", q, "</p>"]
+        questionE = concat ["<p style=\"color:red;\">", q, "</p>"]
+        input     = concat ["<input name=", show $ fst i, ">"]
+
 getQuestionHtml :: FQuestion -> [String]
-getQuestionHtml (i, q) = [question, input]
-  where question = concat ["<p>", q, "</p>"]
-        input    = concat ["<input name=", i, ">"]
+getQuestionHtml q = getQuestionHtmlE (q, True)
 
 -- | A html submit button.
 getOKHtml :: [String]
@@ -91,7 +137,7 @@ getTraceHtml = zipWith (curry getItemHtml) [0..]
 getFormInputHtml :: Form -> [String]
 getFormInputHtml f = [getInputHtml formInputID (show $ map fst f)]
 
--- | Generate hidden input html element with a number as name attribute.  
+-- | Generate hidden input html element with a number as name attribute.
 getItemHtml :: (Int, Item Answers) -> String
 getItemHtml (i, item) = getInputHtml (show i) (show item)
 
@@ -109,7 +155,7 @@ parseTrace :: [String] -> Trace Answers
 parseTrace = map parse
   where parse s = read $ decode s
 
--- | Gather all hidden elements in a html form (byteString) to be used 
+-- | Gather all hidden elements in a html form (byteString) to be used
 -- to generate traces.
 getRawTrace :: ActionM [String]
 getRawTrace = getRawTrace' 0
@@ -127,20 +173,21 @@ getRawTrace = getRawTrace' 0
     packInt i = pack $ show i
 
 -- | Parses html form input names to list of names.
-parseFormInputNames :: String -> [String]
-parseFormInputNames s = read $ decode s
+parseFormInputInfo :: String -> [(ID, InputType)]
+parseFormInputInfo info = read $ decode info
 
 -- | Parse the values from the named elements,
 -- returning them as a ActionM monad.
-parseFormInputValues :: [String] -> ActionM (Item Answers)
-parseFormInputValues names = do
-  values <- mapM f names
-  return $ Answer $ zip names values
-  where f n = getInput $ pack n
+getFormInputValues :: [QInfo] -> ActionM (Item Answers)
+getFormInputValues info = do
+  values <- mapM f info
+  liftIO $ print values
+  return $ Answer $ zip info values
+  where f i = getInput $ pack $ fst i
 
 -- | Gets input from byteString
-getRawFormInput :: ActionM String
-getRawFormInput = getInput $ pack formInputID
+getRawFormInfo :: ActionM String
+getRawFormInfo = getInput $ pack formInputID
 
 -- | Form input string.
 formInputID :: String
